6634e790ecdb8cb85bd4ed105133f1a20021e315
Brandon Hostetter <brandonhostetter05@gmail.com>
2018-09-08 15:47:41 -0400

    Add login and logout ability

--- FirebaseChatApp/LoginViewController.swift
+++ FirebaseChatApp/LoginViewController.swift
@@ -15,13 +15,18 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var profileImageView: UIImageView!
     
+    @IBOutlet weak var loginRegisterSegmentedControl: UISegmentedControl!
+    
     @IBOutlet weak var inputContainerView: UIView!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
     @IBOutlet weak var registerButton: UIButton!
-
+    
+    @IBOutlet weak var inputContainerHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var nameTextFieldHeightConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,7 +43,26 @@ class LoginViewController: UIViewController {
         return .lightContent
     }
     
-    @IBAction func registerButtonTapped(_ sender: Any) {
+    private func setupSegmentedControl() {
+        
+    }
+    
+    private func handleLogin() {
+        guard let email = emailTextField.text, let password = passwordTextField.text else {
+            return
+        }
+
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if error != nil {
+                print(error ?? "Error")
+                return
+            }
+            
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    private func handleRegister() {
         guard let name = nameTextField.text, let email = emailTextField.text, let password = passwordTextField.text else {
             return
         }
@@ -58,7 +82,37 @@ class LoginViewController: UIViewController {
                 "name": name,
                 "email": email
             ]
-            self.db.collection("users").document("\(uid)").setData(user)
+            self.db.collection("users").document("\(uid)").setData(user) { err in
+                if let err = err {
+                    print("Error writing document: \(err)")
+                } else {
+                    print("User successfully added to database!")
+                    
+                    self.dismiss(animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    @IBAction func registerButtonTapped(_ sender: Any) {
+        if loginRegisterSegmentedControl.selectedSegmentIndex == 0 {
+            handleLogin()
+        } else {
+            handleRegister()
+        }
+    }
+    
+    @IBAction func loginRegisterSegmentedControlChanged(_ sender: Any) {
+        if loginRegisterSegmentedControl.selectedSegmentIndex == 0 {
+            // Login
+            registerButton.setTitle("Login", for: .normal)
+            inputContainerHeightConstraint.constant = 100
+            nameTextFieldHeightConstraint.constant = 0
+        } else {
+            // Register
+            registerButton.setTitle("Register", for: .normal)
+            inputContainerHeightConstraint.constant = 150
+            nameTextFieldHeightConstraint.constant = 50
         }
     }
 }
--- FirebaseChatApp/ViewController.swift
+++ FirebaseChatApp/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
@@ -15,9 +16,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(logout))
+        
+        if Auth.auth().currentUser?.uid == nil {
+            perform(#selector(logout), with: nil, afterDelay: 0)
+        }
     }
 
     @objc private func logout() {
+        do {
+            try Auth.auth().signOut()
+        } catch let logoutError {
+            print(logoutError)
+        }
+        
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let controller = storyboard.instantiateViewController(withIdentifier: kLoginViewControllerId)
         self.present(controller, animated: true, completion: nil)
